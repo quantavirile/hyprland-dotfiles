dd6ec5d1ecd4d27bea16fc7341ad839be09c4318
quantavirile <siesta6129@proton.me>
2026-05-04 21:08:30 +0200

    Initial commit

---
+++ dotfiles/.config/mpv/scripts/youtube-search.lua
@@ -0,0 +1,34 @@
+--
+-- Based on 'mpv-zenity-open-files'(https://github.com/alifarazz/mpv-zenity-open-files)
+--
+-- Default keybindings:
+--      CTRL+SHIFT+s: search video in youtube.
+--
+table.unpack = table.unpack or unpack
+utils = require 'mp.utils'
+limit = 5
+
+function invoke_zenity(args) 
+    return utils.subprocess({
+        args = { 'zenity', table.unpack(args) },
+        cancellable = false,
+    })
+end
+
+function search_youtube()
+    local url_select = invoke_zenity({
+        '--entry',
+        '--title=Search Youtube',
+        '--icon=mpv',
+        '--text=Enter Search Text:'
+    })
+    if (url_select.status ~= 0) then
+        return
+    end
+
+    for filename in string.gmatch(url_select.stdout, '[^\n]+') do
+        mp.commandv('loadfile', "ytdl://ytsearch" .. limit .. ":" .. filename, 'append')
+    end
+end
+
+mp.add_key_binding("CTRL+SHIFT+s", "search_youtube", search_youtube)
